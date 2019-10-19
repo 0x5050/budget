1309e4a7ae15831150e2eb15adc13f108a211c3b
damiancierlicki <damiancierlicki@gmail.com>
2019-10-19 15:49:12 +0200

    Created select atom

---
+++ components/atoms/select.vue
@@ -0,0 +1,34 @@
+<template>
+  <select
+    v-model="_value"
+    class="a-select"
+  >
+    <option
+      v-for="option of options"
+      :key="option.value"
+      :disabled="option.value === undefined"
+    >
+      {{ option.label }}
+    </option>
+  </select>
+</template>
+
+<script>
+export default {
+  name: 'ASelect',
+  props: {
+    value: { type: Number, default: null },
+    options: { type: Array, default: () => [] }
+  },
+  computed: {
+    _value: {
+      get () {
+        return this.value
+      },
+      set (value) {
+        this.$emit('input', value)
+      }
+    }
+  }
+}
+</script>
