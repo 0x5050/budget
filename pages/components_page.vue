796d58eb94c925217a6419a64006c0c3982c455f
damiancierlicki <damiancierlicki@gmail.com>
2019-10-19 04:34:36 +0200

    Created basic page for tests

---
+++ pages/components_page.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    Input with validation (max 5 chars)
+    <m-input
+      placeholder="Name"
+      :validation-message="validation"
+      :value="text"
+      @input="setText"
+    />
+    input: {{ text }}
+  </div>
+</template>
+
+<script>
+import MInput from '../components/molecules/validated-input'
+
+export default {
+  name: 'PAtomsPage',
+  components: { MInput },
+  data: () => ({
+    text: '',
+    validationMessage: '',
+  }),
+  computed: {
+    validation() {
+      const max_chars = 5
+      return this.text.length >= max_chars ? 'Error message' : ''
+    }
+  },
+  methods: {
+    setText (text) {
+      this.text = text
+    }
+  },
+}
+</script>
