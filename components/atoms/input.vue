d94f8b410f093dca1a065d19caa667c52d3dbf6d
damiancierlicki <damiancierlicki@gmail.com>
2019-10-19 04:31:35 +0200

    Created input atom

---
+++ components/atoms/input.vue
@@ -0,0 +1,29 @@
+<template>
+  <input
+    v-model="_value"
+    class="a-input"
+    :class="classFeature"
+    :placeholder="placeholder"
+  >
+</template>
+
+<script>
+export default {
+  name: 'AInput',
+  props: {
+    value: { type: String, default: '' },
+    placeholder: { type: String, default: '' },
+    classFeature: { type: String, default: '' },
+  },
+  computed: {
+    _value: {
+      get() {
+        return this.value
+      },
+      set(value) {
+        this.$emit('input', value)
+      },
+    },
+  }
+}
+</script>
