dd66ac02a979fc1df8d2340fc3dbc3d5b9d373f5
Michu <michubilu@wp.pl>
2019-10-22 14:44:54 +0200

    Added firebase

---
+++ functions/nuxt/router.js
@@ -0,0 +1,28 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import { interopDefault } from './utils'
+import scrollBehavior from './router.scrollBehavior.js'
+
+const _505df0ec = () => interopDefault(import('../../src/pages/index.vue' /* webpackChunkName: "pages/index" */))
+
+Vue.use(Router)
+
+export const routerOptions = {
+  mode: 'history',
+  base: decodeURI('/'),
+  linkActiveClass: 'nuxt-link-active',
+  linkExactActiveClass: 'nuxt-link-exact-active',
+  scrollBehavior,
+
+  routes: [{
+      path: "/",
+      component: _505df0ec,
+      name: "index"
+    }],
+
+  fallback: false
+}
+
+export function createRouter() {
+  return new Router(routerOptions)
+}
--- src/server/index.js
+++ src/server/index.js
@@ -1,8 +1,10 @@
+const express = require('express')
 const consola = require('consola')
-const { Nuxt, Builder } = require('nuxt')\
+const { Nuxt, Builder } = require('nuxt')
+const app = express()
 
 // Import and Set Nuxt.js options
-const config = require('../nuxt.config.js/index.js')
+const config = require('../nuxt.config.js')
 config.dev = process.env.NODE_ENV !== 'production'
 
 async function start () {
@@ -19,6 +21,11 @@ async function start () {
     await nuxt.ready()
   }
 
+  // Give nuxt middleware to express
+  app.use(nuxt.render)
+
+  // Listen the server
+  app.listen(port, host)
   consola.ready({
     message: `Server listening on http://${host}:${port}`,
     badge: true
